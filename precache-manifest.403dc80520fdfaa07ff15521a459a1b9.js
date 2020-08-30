611755dc093c8fb4221cf38ba2c5af9ca40d1b69
khunshan97 <khunshan.butt@gmail.com>
2020-08-31 03:31:25 +0500

    Updates

---
+++ precache-manifest.403dc80520fdfaa07ff15521a459a1b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f62d048cef42fc1fc8143777a5ba78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "575db00d3c327be996b2",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0044a27d8729b4182a7e",
+    "url": "/static/css/main.e9084563.chunk.css"
+  },
+  {
+    "revision": "575db00d3c327be996b2",
+    "url": "/static/js/2.45dadcb6.chunk.js"
+  },
+  {
+    "revision": "b6840d1fa4cd3c3e88b7c215b28b2e9e",
+    "url": "/static/js/2.45dadcb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0044a27d8729b4182a7e",
+    "url": "/static/js/main.be690e70.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "f752341a0b4adfdc8e280250f00c6474",
+    "url": "/static/media/resume.f752341a.pdf"
+  }
+]);
